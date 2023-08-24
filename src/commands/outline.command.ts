f027eda1e9a4ef3e1db294e6a12d5166f1014864
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-08-24 17:59:04 +1000

    cache the subject outlines in db

--- src/commands/outline.command.ts
+++ src/commands/outline.command.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
 import { AttachmentBuilder } from 'discord.js';
 import {
   Context,
@@ -8,10 +9,92 @@ import {
 } from 'necord';
 import { OutlineCommandDto } from 'src/dto/OutlineCommandDto';
 import mainLogger from 'src/logger';
+import type { Cache } from 'cache-manager';
 
 @Injectable()
 export class OutlineCommand {
   private readonly logger = mainLogger.scope(OutlineCommand.name);
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  /**
+   * Get the subject outline for a specific subject from the UTS API or cache
+   * @param subjectCode The subject code to get the outline for
+   * @param session The session to get the outline for
+   * @param year The year to get the outline for
+   * @returns The outline as an attachment, or null if it could not be found
+   */
+  private async getOutline(
+    subjectCode: number,
+    session: string,
+    year: number,
+  ): Promise<AttachmentBuilder | null> {
+    const fileName = `${subjectCode}-${session ?? 'AUT'}-${
+      year ?? new Date().getFullYear()
+    }-Subject_Outline.pdf`;
+
+    const cachedOutline = await this.cacheManager.get<string>(fileName);
+
+    if (cachedOutline) {
+      this.logger.debug('Found cached outline', fileName);
+      const arrayBuffer = Buffer.from(cachedOutline, 'base64');
+
+      const attachment = new AttachmentBuilder(arrayBuffer)
+        .setName(fileName)
+        .setDescription(
+          `Subject outline for ${subjectCode} ${session} ${year}`,
+        );
+
+      return attachment;
+    } else {
+      const urlSearch = new URLSearchParams();
+
+      urlSearch.set('lastGenerated', 'true');
+      urlSearch.set('subjectCode', subjectCode.toString());
+      urlSearch.set('mode', 'standard');
+      urlSearch.set('location', 'city');
+      urlSearch.set('session', session);
+
+      if (year) {
+        urlSearch.set('year', year.toString());
+      } else {
+        const currentYear = new Date().getFullYear();
+        urlSearch.set('year', currentYear.toString());
+      }
+
+      const url = `https://cis-admin-api.uts.edu.au/subject-outlines/index.cfm/PDFs?${urlSearch.toString()}#view=FitH`;
+      const fetchedOutline = await fetch(url, {
+        headers: {
+          'User-Agent': 'UTS-Subject-Outline-Bot',
+        },
+      });
+
+      if (
+        !fetchedOutline.ok ||
+        !fetchedOutline.headers.get('Content-Type').includes('application/pdf')
+      ) {
+        return null;
+      }
+
+      const arrayBuffer = await fetchedOutline.arrayBuffer();
+
+      const buffer = Buffer.from(arrayBuffer);
+
+      await this.cacheManager.set(
+        fileName,
+        buffer.toString('base64'),
+        1000 * 60 * 60 * 24 * 7, // 7 days
+      );
+
+      const attachment = new AttachmentBuilder(buffer)
+        .setName(fileName)
+        .setDescription(
+          `Subject outline for ${subjectCode} ${session} ${year}`,
+        );
+
+      return attachment;
+    }
+  }
 
   @SlashCommand({
     name: 'outline',
@@ -22,61 +105,19 @@ export class OutlineCommand {
     @Options()
     { subjectCode, session, year }: OutlineCommandDto,
   ) {
-    await interaction.deferReply();
-    const urlSearch = new URLSearchParams();
+    const outlineAttachment = await this.getOutline(subjectCode, session, year);
 
-    urlSearch.set('lastGenerated', 'true');
-    urlSearch.set('subjectCode', subjectCode.toString());
-    urlSearch.set('mode', 'standard');
-    urlSearch.set('location', 'city');
-    urlSearch.set('session', session);
-
-    if (year) {
-      urlSearch.set('year', year.toString());
-    } else {
-      const currentYear = new Date().getFullYear();
-      urlSearch.set('year', currentYear.toString());
-    }
-
-    const url = `https://cis-admin-api.uts.edu.au/subject-outlines/index.cfm/PDFs?${urlSearch.toString()}#view=FitH`;
-
-    const fetchedOutline = await fetch(url, {
-      headers: {
-        'User-Agent': 'UTS-Subject-Outline-Bot',
-      },
-    });
-
-    if (!fetchedOutline.ok) {
-      await interaction.followUp({
-        content: 'Sorry, I could not find that subject outline.',
+    if (!outlineAttachment) {
+      await interaction.reply({
+        content: 'Could not find subject outline!',
         ephemeral: true,
       });
       return;
     }
 
-    if (
-      !fetchedOutline.headers.get('Content-Type').includes('application/pdf')
-    ) {
-      await interaction.followUp({
-        content: 'Sorry, I could not find that subject outline.',
-        ephemeral: true,
-      });
-      return;
-    }
-
-    const arrayBuffer = await fetchedOutline.arrayBuffer();
-
-    const fileName = `${subjectCode}-${session ?? 'AUT'}-${
-      year ?? new Date().getFullYear()
-    }-Subject_Outline.pdf`;
-
-    const attachment = new AttachmentBuilder(Buffer.from(arrayBuffer))
-      .setName(fileName)
-      .setDescription(`Subject outline for ${subjectCode} ${session} ${year}`);
-
-    await interaction.followUp({
+    await interaction.reply({
       content: 'Here is your subject outline!',
-      files: [attachment],
+      files: [outlineAttachment],
       ephemeral: true,
     });
   }
--- src/discord.module.ts
+++ src/discord.module.ts
@@ -4,13 +4,31 @@ import { NecordModule } from 'necord';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PingCommand } from './commands/ping.command';
 import { InviteCommand } from './commands/invite.command';
+import { CacheModule } from '@nestjs/cache-manager';
 import { OutlineCommand } from './commands/outline.command';
+import { redisStore } from 'cache-manager-ioredis-yet';
+
+import type { RedisOptions } from 'ioredis';
 // import { InjectDynamicProviders } from 'nestjs-dynamic-providers';
 
 // @InjectDynamicProviders({ pattern: 'dist/commands/**/*.command.js' })
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync<RedisOptions>({
+      useFactory: (configService: ConfigService) => {
+        const redisUrl = new URL(configService.getOrThrow<string>('REDIS_URL'));
+        return {
+          store: redisStore,
+          host: redisUrl.hostname,
+          port: parseInt(redisUrl.port),
+          password: redisUrl.password,
+          db: 0,
+        };
+      },
+      inject: [ConfigService],
       isGlobal: true,
     }),
     NecordModule.forRootAsync({
