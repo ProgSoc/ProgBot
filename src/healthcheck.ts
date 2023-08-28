f79f26da13b17e75d452f73e50207b873c5c0974
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-08-28 19:08:25 +1000

    Revert "fix(core): Use curl for health checks"
    This reverts commit d28b693d3db7421df3f87b3e78cbaf7fa5954621.

---
+++ src/healthcheck.ts
@@ -0,0 +1,13 @@
+const bootstrap = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/health');
+    if (!response.ok) {
+      process.exit(1);
+    } else {
+      process.exit(0);
+    }
+  } catch (error) {
+    process.exit(1);
+  }
+};
+bootstrap();
