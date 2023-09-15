003d5a0ad99222b8e0d07ce0597ab2f477f14b7f
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-09-15 14:20:20 +1000

    uptime command

---
+++ src/commands/uptime.command.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { DateTime, Duration } from 'luxon';
+import { Context, SlashCommand, type SlashCommandContext } from 'necord';
+import mainLogger from 'src/logger';
+
+@Injectable()
+export class UptimeCommand {
+  private readonly logger = mainLogger.scope(UptimeCommand.name);
+
+  @SlashCommand({ name: 'uptime', description: 'Get the bot uptime' })
+  public async ping(@Context() [interaction]: SlashCommandContext) {
+    const uptime = process.uptime();
+
+    const relativeUptime = Duration.fromMillis(uptime * 1000).toHuman();
+
+    this.logger.info('Uptime command received');
+
+    await interaction.reply({
+      content: `The bot has been up for ${relativeUptime}`,
+      ephemeral: true,
+    });
+  }
+}
