41e72d5074fee0bd1d42b0a40c9a4e48c962dce0
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-09-15 14:56:48 +1000

    add announcement channel to guild settings

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,17 +1,8 @@
 import { pgTable, text, unique } from 'drizzle-orm/pg-core';
 
-export const subject = pgTable('subject', {
+export const guilds = pgTable('guilds', {
+  /** The Guild Id */
   id: text('id').primaryKey(),
-});
-
-export const major = pgTable('major', {
-  id: text('id').primaryKey(),
-});
-
-export const submajor = pgTable('submajor', {
-  id: text('id').primaryKey(),
-});
-
-export const course = pgTable('course', {
-  id: text('id').primaryKey(),
+  /** The announcement channel Id */
+  announcementChannel: text('announcement_channel'),
 });
