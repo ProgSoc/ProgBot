d0ed7c692d9f54b903dde61962b7a4ef59e0973a
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-09-18 14:55:57 +1000

    highlighting is a pain

--- src/commands/docs.command.ts
+++ src/commands/docs.command.ts
@@ -50,6 +50,53 @@ export class DocsCommand {
 }
 
 const searchResultToEmbed = (result: SearchResult) => {
+  /**
+ * The metadata contains a key for each search term found in the document and the field in which it was found. This will contain all the metadata about this term and field; for example the position of the term matches:
+ * e.g. 
+  "metadata": {
+    "test": {
+      "body": {
+        "position": [[0, 4], [24, 4]]
+      }
+    }
+  }
+*/
+
+  /**
+   * We want to highlight the search terms in the embed, so we need to get the positions of the search terms in the text
+   * { text: [ [ 580, 4 ], [ 808, 4 ], [ 1153, 7 ], [ 1241, 4 ] ] }
+   * It includes the position of the term and the length of the term
+   */
+  const positionsToHighlightByField = Object.entries(result.meta).reduce(
+    (acc, [term, fields]) => {
+      Object.entries(fields).forEach(([field, { position }]) => {
+        if (!acc[field]) {
+          acc[field] = [];
+        }
+        acc[field].push(...position);
+      });
+      return acc;
+    },
+    {} as Record<string, [number, number][]>,
+  );
+
+  // const highlightedText = Object.entries(positionsToHighlightByField).reduce(
+  //   (acc, [field, positions]) => {
+  //     const sortedPositions = positions.sort(([a], [b]) => a - b);
+  //     let lastPosition = 0;
+  //     sortedPositions.forEach(([position, length]) => {
+  //       acc += result[field].slice(lastPosition, position);
+  //       acc += `**${result[field].slice(position, position + length)}**`;
+  //       lastPosition = position + length;
+  //     });
+  //     acc += result[field].slice(lastPosition);
+  //     return acc;
+  //   },
+  //   '',
+  // );
+
+  // console.log(highlightedText);
+
   const embed = new EmbedBuilder()
     .setTitle(result.title)
     .setDescription(result.text.split('\n').slice(0, 2).join('\n'))
